30182802df47cb445404f79627504ff2665a3974
dina <dinaisakova10@gmail.com>
2026-05-12 10:20:59 +0900

    feat: learn python objects

---
+++ objects.py
@@ -0,0 +1,30 @@
+'''
+OBJECTS 
+      whats objects 
+      iterabe objects & range 
+      dictionary
+      error handling systems 
+
+'''
+
+import array  # package/module
+import math  # package
+from math import ceil
+print("======Whats object ==========")
+# An object has state and properties
+# Everything object in Python
+
+print(type('hello world '))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# paradigma > OOP & functional programming
+# OOP 4 concepts > Abstractions, encapculation, inheritance , polimorophism
+
+result1 = math.ceil(97.7)  # call
+print("result1", result1)
+
+result2 = ceil(98.7)
+print("result2", result2)
